add9424c159178513555990d333c1745bead9c19
Aditya Wiraha <squalvj@gmail.com>
2019-08-03 03:09:27 +0800

    integrate with react native debugger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-import devTools from 'remote-redux-devtools';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './../reducers';
 
 export default function configureStore(onCompletion){
-  const enhancer = compose(
-    applyMiddleware(thunk),
-    devTools({
-      name: 'Bibit',
-      port: 8081,
-      realtime: true,
-    })
+  const enhancer = composeWithDevTools(
+    applyMiddleware(thunk)
   );
 
   const store = createStore(reducer, enhancer);
